e62b22e1249ee851625a09f583bea952b0987315
Ajay Ranipeta <ajay.ranipeta@gmail.com>
2016-10-26 16:37:23 -0400

    Update Gruntfile to switch the URL back to 'indicators'

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,7 +16,7 @@ module.exports = function(grunt) {
             remove: ['.dev'],
             append: [
               {selector:'head',html:'<base href="//mapoflife.github.io/' + pkg.base + '/" />'},
-              {selector:'head',html:'<script>if (window.location.pathname.indexOf("indicators") > 0){window.location.href = window.location.href.replace("indicators", "EbbeNielsen");}</script>'},
+              {selector:'head',html:'<script>if (window.location.pathname.indexOf("EbbeNielsen") > 0){window.location.href = window.location.href.replace("EbbeNielsen", "indicators");}</script>'},
               {selector:'head',html:'<link href="static/app.min.css" rel="stylesheet">'},
               {selector:'head',html:'<script src="static/app.min.js"></script>'}
             ]
@@ -65,7 +65,7 @@ module.exports = function(grunt) {
      },
      pages: {
        options: {
-         remote: pkg.altRepository,
+         remote: pkg.repository,
          force:true,
          branch: 'gh-pages'
        }
